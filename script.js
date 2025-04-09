2d70aec5618f377548ab2d749f2b9cd04dcbcf66
JenniferLee <jennifer.lee24@student.cart.org>
2025-04-09 08:05:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const userInput = document.getElementById('text-input');
+// ^related to the text-input from the html
 const checkPalindromeBtn = document.getElementById('check-btn');
+// ^related to the check-btn from the html
 const resultDiv = document.getElementById('result');
+// ^related to the result from the html
 
 const checkPalindrome = (input) => {
     const originalInput = input;
-
+// ^checkPalindrome
     if (input === '') {
         alert('Please input a value');
         return;
     }
 
     resultDiv.replaceChildren();
-
+// ^replaceChildren
     const lowerCaseStr = input.replace(/[^A-Za-z0-9]/gi, '').toLowerCase(); // remove non alphanumeric characters
     let resultMessage = `<strong>${originalInput}</strong> ${
         lowerCaseStr === [...lowerCaseStr].reverse().join('') ? 'is' : 'is not'
